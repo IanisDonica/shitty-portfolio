cf961ca6e285af7cab753111851923fadd9b99a2
IanisDonica <64953588+IanisDonica@users.noreply.github.com>
2022-04-13 13:12:05 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,95 @@
+window.addEventListener("mousemove", cursorMove);
+
+function cursorMove(event) {
+    var el = document.querySelector(".cursor");
+    el.style.top = event.clientY + "px";
+    el.style.left = event.clientX + "px";
+}
+
+window.addEventListener("mouseover", cursorMove2); 
+
+function cursorMove2(event) {
+    var el2 = document.querySelector(".cursor2");
+    el2.style.top = event.clientY + "px";
+    el2.style.left = event.clientX + "px";
+}
+
+var bars1 = document.querySelector(".bars1");
+
+// bars1.addEventListener("mousemove", bars1Move);
+
+// function bars1Move(event) {
+//     document.querySelector(".cursor_projects").style.display = "block";
+//     var elbars1 = document.querySelector(".cursor_projects");
+//     elbars1.style.top = event.clientY + "px";
+//     elbars1.style.left = event.clientX + "px";
+// } 
+
+// setTimeout(barstimeout1, 5);
+
+// function barstimeout1() {
+//     bars1.addEventListener("mouseover", bars1Move);
+
+//     function bars1Move(event) {
+//         document.querySelector(".cursor_projects").style.display = "block";
+    
+//         var elbars1 = document.querySelector(".cursor_projects");
+//         elbars1.style.top = event.clientY + "px";
+//         elbars1.style.left = event.clientX + "px";
+//     } 
+//     setTimeout(barstimeout1, 5);
+// }
+
+var k = 0;
+var numSections = 5;
+var isAnimationFired = new Array(numSections).fill(false);
+
+new fullpage('#fullpage', {
+    onLeave: function(origin, destination, direction) {
+        
+        if(!isAnimationFired[destination.index]){
+            if(destination.index === 1) {
+                if(k == 0) {
+                    const animationPhone2 = window.matchMedia("(max-width: 1000px)")
+                    let tl2 = gsap.timeline() 
+
+                    if (animationPhone2.matches) {
+                        tl2.from(".drive", {
+                            duration: 1.5,
+                            y: -100,
+                            opacity: 0,
+                        })   
+                    }
+                    else {
+                        tl2.from(".drive", {
+                            duration: 1.5,
+                            y: -100,
+                            opacity: .0,
+                        })   
+                        tl2.from(".SR2R", {
+                            height: 0,
+                            duration: 2,
+                        })
+                        tl2.from(".SR1R", {
+                            height: 0,
+                            duration: 2,
+                        }, "<")
+                        tl2.from(".SR2T", {
+                            width: 0,
+                            duration: 2,
+                        }, "<")
+                        tl2.from(".services_pag", {
+                            duration: 1,
+                            opacity: 0,
+                            stagger: 0.75,
+                            ease: Power0.easeNone,
+                        }, "<" )
+                    }
+                }
+                isAnimationFired[destination.index] = true;
+            }
+    }}
+});
 
 let tl1 = gsap.timeline()
 
@@ -69,12 +161,19 @@ else {
         opacity: .0,
         delay: 1
     })
-
+    tl1.from(".cursor", {
+        opacity: 0,
+        duration: 2,
+    })
+    tl1.from(".cursor2", {
+        opacity: 0,
+        duration: 2,
+    }, "<")
     tl1.from(".R1T, .R1B", {
         height: 0,
         duration: 2,
         ease: Power0.easeNone,
-    })
+    }, "<")
     tl1.from(".R1M", {
         width: 0,
         duration: 4,
@@ -103,13 +202,14 @@ else {
 
 }
 
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
-let tl2 = gsap.timeline()
+// let tl2 = gsap.timeline()
 
-tl2.from(".drive", {
-    duration: 1.5,
-    y: -50,
-    opacity: .0,
-    delay: 1
-})
+// tl2.from(".drive", {
+//     duration: 1.5,
+//     y: -50,
+//     opacity: .0,
+//     delay: 1
+// })
+
